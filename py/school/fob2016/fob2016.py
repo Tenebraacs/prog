13996017a031e512701552891faf156f58d0482c
Karacsony Zoltan <karacsony.zolika@gmail.com>
2023-03-22 08:20:50 +0100

    A fob2016 befejezese

--- py/school/fob2016/fob2016.py
+++ py/school/fob2016/fob2016.py
@@ -13,6 +13,7 @@ def fob2016 (bemenet, mod):
 
     indulok = [lista[i] for i in range(0, len(lista), 11)]
     #print("Az indulok: ", indulok)
+
     print("Az indulok szama: ", len(indulok))
 
 
@@ -23,14 +24,26 @@ def fob2016 (bemenet, mod):
             no += 1
     no_szazalek = no / (len(indulok) / 100)
     no_szazalek_formazott = "{:.2f}".format(no_szazalek)
+
     print("A nok szazaleka: ", no_szazalek_formazott, "%")
+    print("\n")
 
 
-    nok = []
+    legjobb = [0, 0]
+    temp = 0
     for index,e in enumerate(lista):
         if e == "Noi":
-            nok.append(lista[index - 1:index + 10])
-    
+            temp = 0
+            for pont in lista[index + 2:index + 10]:
+                temp += int(pont)
+            if temp > legjobb[0]:
+                legjobb.clear()
+                legjobb = [temp, index - 1]
+
+    print("A bajnok noi versenyzo")
+    print("Nev:         ", lista[legjobb[1]])
+    print("Egyesulet:   ", lista[legjobb[1] + 2])
+    print("Osszpont:    ", legjobb[0])
 
 
 
